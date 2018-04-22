2e38b8aab6c29f4db61d0dddd94450b1192dca68
Eddie Shea <ejshea2>
2018-04-22 11:16:17 -0500

    Created a new activity and linked it to a button on the main activity. This new activity will show the most recent launch.

--- app/src/main/java/edu/illinois/cs/cs125/mp7/MainActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/mp7/MainActivity.java
@@ -1,7 +1,10 @@
 package edu.illinois.cs.cs125.mp7;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void newLaunch(View view) {
+        Intent intent = new Intent(this, launch_data.class);
+        startActivity(intent);
     }
 }
