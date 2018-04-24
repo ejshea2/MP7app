33a66ceaeacad8c27c598fe79b90caed0bb8cf58
Eddie Shea <ejshea2>
2018-04-23 22:22:29 -0500

    Changed the font and added some new elements to the launch data activity.

--- app/src/main/java/edu/illinois/cs/cs125/mp7/MainActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/mp7/MainActivity.java
@@ -1,17 +1,25 @@
 package edu.illinois.cs.cs125.mp7;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button recent, past;
+    Typeface tfc;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recent = (Button) findViewById(R.id.get_launch);
+        past = (Button) findViewById(R.id.past_launches);
+        tfc = Typeface.createFromAsset(getAssets(), "fonts/Prototype.ttf");
+        recent.setTypeface(tfc);
+        past.setTypeface(tfc);
     }
 
     public void newLaunch(View view) {
--- app/src/main/java/edu/illinois/cs/cs125/mp7/launch_data.java
+++ app/src/main/java/edu/illinois/cs/cs125/mp7/launch_data.java
@@ -1,29 +1,35 @@
 package edu.illinois.cs.cs125.mp7;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class launch_data extends AppCompatActivity {
-
+    TextView launch_date_num, launch_date_text, launch_num, launch_num_text;
+    Typeface tfc;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch_data);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        launch_num_text = (TextView) findViewById(R.id.launchNumTxt);
+        launch_num = (TextView) findViewById(R.id.launchNum);
+        launch_date_text = (TextView) findViewById(R.id.launchDateTxt);
+        launch_date_num = (TextView) findViewById((R.id.launchDateNum));
+        tfc = Typeface.createFromAsset(getAssets(), "fonts/Prototype.ttf");
+        launch_date_num.setTypeface(tfc);
+        launch_date_text.setTypeface(tfc);
+        launch_num.setTypeface(tfc);
+        launch_num_text.setTypeface(tfc);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
