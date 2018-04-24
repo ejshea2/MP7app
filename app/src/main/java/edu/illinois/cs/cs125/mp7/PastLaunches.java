9a2ff899a5e843df407b5871752eeafa3557e99c
Eddie Shea <ejshea2>
2018-04-24 15:12:21 -0500

    Made the startAPIcall method return a string so we can make it more flexible and useful so we can call it for anything we want from the spacex API.

--- app/src/main/java/edu/illinois/cs/cs125/mp7/PastLaunches.java
+++ app/src/main/java/edu/illinois/cs/cs125/mp7/PastLaunches.java
@@ -1,22 +1,41 @@
 package edu.illinois.cs.cs125.mp7;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class PastLaunches extends AppCompatActivity {
-
+    Button b1, b2, b3, b4, b5, b6, b7, b8;
+    Typeface tfc;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_past_launches);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        b1 = (Button) findViewById(R.id.button2);
+        b2 = (Button) findViewById(R.id.button3);
+        b3 = (Button) findViewById(R.id.button4);
+        b4 = (Button) findViewById(R.id.button5);
+        b5 = (Button) findViewById(R.id.button6);
+        b6 = (Button) findViewById(R.id.button7);
+        b7 = (Button) findViewById(R.id.button8);
+        b8 = (Button) findViewById(R.id.button9);
+        tfc = Typeface.createFromAsset(getAssets(), "fonts/Prototype.ttf");
+        b1.setTypeface(tfc);
+        b2.setTypeface(tfc);
+        b3.setTypeface(tfc);
+        b4.setTypeface(tfc);
+        b5.setTypeface(tfc);
+        b6.setTypeface(tfc);
+        b7.setTypeface(tfc);
+        b8.setTypeface(tfc);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/edu/illinois/cs/cs125/mp7/launch_data.java
+++ app/src/main/java/edu/illinois/cs/cs125/mp7/launch_data.java
@@ -6,12 +6,27 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class launch_data extends AppCompatActivity {
+    /** Default logging tag for messages from the main activity. */
+    private static final String TAG = "MP7:main";
+
+    /** Request queue for our API requests. */
+    private static RequestQueue requestQueue;
+
     TextView launch_date_num, launch_date_text, launch_num, launch_num_text;
     Typeface tfc;
     @Override
@@ -29,7 +44,37 @@ public class launch_data extends AppCompatActivity {
         launch_date_text.setTypeface(tfc);
         launch_num.setTypeface(tfc);
         launch_num_text.setTypeface(tfc);
+        startAPIcall("rocket_id");
+    }
 
+    String startAPIcall(final String fieldName) {
+        try {
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                    Request.Method.GET,
+                    "https://api.spacexdata.com/v2/launches/latest",
+                    null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(final JSONObject response) {
+                            try {
+                                Log.d(TAG, "HERERERERERERERERER" + response.toString(2));
+
+
+                            } catch (JSONException ignored) { }
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(final VolleyError error) {
+                    Log.e(TAG, error.toString());
+                }
+            });
+            String json = jsonObjectRequest.toString();
+            Log.d(TAG, json);
+            return json;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
 }
